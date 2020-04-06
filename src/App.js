14b4066f136c77239f7a0a43bbd212f1f17dbb11
Bartek <bartlomiej@bystrzycki.com>
2020-04-06 16:37:36 +0200

    functions vs arrow functions

--- src/App.js
+++ src/App.js
@@ -10,12 +10,18 @@ class App extends Component {
       monsters: [],
       searchField: ""
     };
+
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then(response => response.json())
       .then(users => this.setState({ monsters: users }));
+  }
+
+  handleChange(e) {
+    this.setState({ searchField: e.target.value });
   }
 
   render() {
@@ -29,10 +35,7 @@ class App extends Component {
 
     return (
       <div className="App">
-        <SearchBox
-          placeholder="search cats"
-          handleChange={e => this.setState({ searchField: e.target.value })}
-        />
+        <SearchBox placeholder="search cats" handleChange={this.handleChange} />
         <CardList monsters={filteredMonsters} />
       </div>
     );
